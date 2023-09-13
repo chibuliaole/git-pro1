c6b34e3eb7f3ea1b933bbe7b345126dd22d273b0
chibuliaole <13419977+chibuliaole@user.noreply.gitee.com>
2023-09-13 08:20:15 +0000

    update src/main/java/com/maomao/Main.java. 改写 main
    Signed-off-by: chibuliaole <13419977+chibuliaole@user.noreply.gitee.com>

--- src/main/java/com/maomao/Main.java
+++ src/main/java/com/maomao/Main.java
@@ -3,5 +3,6 @@ package com.maomao;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("hello gitte");
     }
 }
